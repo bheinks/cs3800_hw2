52d2118b776210f016f2ad46e35e91042820d511
Brett Heinkel <bhbn8@mst.edu>
2017-04-03 15:50:04 -0500

    take in programlist and create page tables

--- memorysimulator.py
+++ memorysimulator.py
@@ -2,6 +2,17 @@
 
 import argparse
 from os.path import isfile
+from pprint import pprint
+
+AVAILABLE_FRAMES = 512
+
+class Program:
+    def __init__(self, number, size, pages_needed, start_loc):
+        self.number = number
+        self.size = size
+        self.pages_needed = pages_needed
+        self.start_loc = start_loc
+        self.end_loc = start_loc + pages_needed - 1 # starting from 0, hence -1
 
 # argparse "type" to determine if a file exists
 def file_exists(filename):
@@ -15,11 +26,35 @@ def main():
     # argparse performs all of the necessary checks on input formatting
     parser.add_argument("programlist", type=file_exists)
     parser.add_argument("programtrace", type=file_exists)
-    parser.add_argument("page size", type=int, choices=[2**i for i in range(5)])
-    parser.add_argument("page replacement algorithm", choices=["clock", "fifo", "lru"])
-    parser.add_argument("demand/prepaging", type=int, choices=[0, 1])
+    parser.add_argument("page_size", type=int, choices=[2**i for i in range(5)], help="page size")
+    parser.add_argument("algo", choices=["clock", "fifo", "lru"], help="page replacement algorithm")
+    parser.add_argument("paging", type=int, choices=[0, 1], help="demand/prepaging")
 
     args = parser.parse_args()
+
+    page_table = []
+    with open(args.programlist) as f:
+        # index == prog number. filter blank lines
+        programlist = [int(line.split()[1]) for line in f if line.strip()]
+
+    page_tables = create_page_tables(programlist, args.page_size)
+    print_page_tables(page_tables, args.page_size)
+
+def create_page_tables(programlist, page_size):
+    page_tables = []
+    abs_page = 0
+
+    for i, prog_size in enumerate(programlist):
+        pages_needed = (prog_size // page_size) + 1
+        page_tables.append(Program(i, prog_size, pages_needed, abs_page))
+        abs_page += pages_needed
+
+    return page_tables
+
+def print_page_tables(page_tables, page_size):
+    print("{}\t{}\t{}\t{}\t{}".format("no", "size", "page", "need", "loc"))
+    for p in page_tables:
+        print("{}\t{}\t{}\t{}\t{} - {}".format(p.number, p.size, page_size, p.pages_needed, p.start_loc, p.end_loc))
 
 if __name__ == "__main__":
     main()
