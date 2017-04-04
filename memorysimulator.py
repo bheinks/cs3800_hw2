62eb8b5d0751017f5a2d8fec2474071e66157dfc
Brett Heinkel <bheinks@gmail.com>
2017-04-03 23:39:55 -0500

    clarify variable names and fix potential rounding issue

--- memorysimulator.py
+++ memorysimulator.py
@@ -2,14 +2,14 @@
 
 import argparse
 from os.path import isfile
-from pprint import pprint
+from math import ceil
 
 AVAILABLE_FRAMES = 512
 
 class Program:
-    def __init__(self, number, size, pages_needed, start_loc):
-        self.number = number
-        self.size = size
+    def __init__(self, prog_no, prog_size, pages_needed, start_loc):
+        self.prog_no = prog_no
+        self.prog_size = prog_size
         self.pages_needed = pages_needed
         self.start_loc = start_loc
         self.end_loc = start_loc + pages_needed - 1 # starting from 0, hence -1
@@ -27,12 +27,11 @@ def main():
     parser.add_argument("programlist", type=file_exists)
     parser.add_argument("programtrace", type=file_exists)
     parser.add_argument("page_size", type=int, choices=[2**i for i in range(5)], help="page size")
-    parser.add_argument("algo", choices=["clock", "fifo", "lru"], help="page replacement algorithm")
+    parser.add_argument("algorithm", choices=["clock", "fifo", "lru"], help="page replacement algorithm")
     parser.add_argument("paging", type=int, choices=[0, 1], help="demand/prepaging")
 
     args = parser.parse_args()
 
-    page_table = []
     with open(args.programlist) as f:
         # index == prog number. filter blank lines
         programlist = [int(line.split()[1]) for line in f if line.strip()]
@@ -42,19 +41,19 @@ def main():
 
 def create_page_tables(programlist, page_size):
     page_tables = []
-    abs_page = 0
+    abs_page_loc = 0
 
-    for i, prog_size in enumerate(programlist):
-        pages_needed = (prog_size // page_size) + 1
-        page_tables.append(Program(i, prog_size, pages_needed, abs_page))
-        abs_page += pages_needed
+    for prog_no, prog_size in enumerate(programlist):
+        pages_needed = ceil(prog_size / page_size)
+        page_tables.append(Program(prog_no, prog_size, pages_needed, abs_page_loc))
+        abs_page_loc += pages_needed
 
     return page_tables
 
 def print_page_tables(page_tables, page_size):
     print("{}\t{}\t{}\t{}\t{}".format("no", "size", "page", "need", "loc"))
     for p in page_tables:
-        print("{}\t{}\t{}\t{}\t{} - {}".format(p.number, p.size, page_size, p.pages_needed, p.start_loc, p.end_loc))
+        print("{}\t{}\t{}\t{}\t{} - {}".format(p.prog_no, p.prog_size, page_size, p.pages_needed, p.start_loc, p.end_loc))
 
 if __name__ == "__main__":
     main()
