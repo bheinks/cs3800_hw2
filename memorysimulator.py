4b678310ce512d150acbad91a8b93c3e86cc3840
Brett Heinkel <bheinks@gmail.com>
2017-04-05 21:03:19 -0500

    brettify code

--- memorysimulator.py
+++ memorysimulator.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-import queue
 import argparse
-import math
 from os.path import isfile
 from math import ceil
 
@@ -19,12 +17,30 @@ class Program:
         self.end_loc = start_loc + pages_needed - 1 # starting from 0, hence -1
 
 class Progpage:
-    def __init__(self, prognum, relprogpage, location, usebit, tsr):
+    def __init__(self, index, prognum, relprogpage, location, usebit, tsr):
+        self.index = index
         self.prognum = prognum
         self.relprogpage = relprogpage
         self.location = location
-        self.usebit = usebit  #for clock alogorithm
-        self.tsr = tsr #for remove last used
+        self.usebit = usebit # for clock alogorithm
+        self.tsr = tsr # for remove last used
+
+def main(pl_filename, pt_filename, page_size, algorithm, paging):
+    with open(pl_filename) as f:
+        # index == prog number. filter blank lines
+        programlist = [int(line.split()[1]) for line in f if line.strip()]
+
+    with open(pt_filename) as f:
+        programtrace = None
+
+    page_tables = create_page_tables(programlist, page_size)
+    print_page_tables(page_tables, page_size)
+
+    print()
+    
+    # test code for the main memory table
+    mem_table = create_mem(page_tables, page_size)
+    print_mem_table(mem_table)
 
 #create program index files
 def create_page_tables(programlist, page_size):
@@ -39,74 +55,57 @@ def create_page_tables(programlist, page_size):
     return page_tables
 
 #create and allocate main memory
-def create_mem(program_tables):
+def create_mem(page_tables, page_size):
     mem_table = []
     page_file_size = AVAILABLE_FRAMES / page_size
-    page_per_program = page_file_size // len(program_tables)
-    for prognum in range(0, len(program_tables)):
-        for y in range(0, int(page_per_program)):
-            mem_table.append(Progpage(prognum, y ,program_tables[prognum].start_loc + y, 1, 0))
+    page_per_program = int(page_file_size // len(page_tables))
+    index = 0
+
+    for program in page_tables:
+        for page in range(page_per_program):
+            mem_table.append(Progpage(index, program.prog_no, page, program.start_loc + page, 1, 0))
+            index += 1
+
     return mem_table
 
-# argparse "type" to determine if a file exists
-def file_exists(filename):
-    if not isfile(filename):
-        raise argparse.ArgumentTypeError("{} does not exist".format(filename))
-    return filename
-
 #First in first out algorithm
-def FiFo():
-    queue = queue.Queue()  #Queue for FiFo
+def fifo():
     return
 
 #Clock algorithm
-def Clock():
+def clock():
     return
 
 #Last recently used algorithm
-def LRU():
+def lru():
     return
 
-def main():
-    parser = argparse.ArgumentParser(description = "Simulate a virtual memory management system with various page sizes, paging replacement algorithms, and demand/prepaging.")
-
-    # argparse performs all of the necessary checks on input formatting
-    parser.add_argument("programlist", type=file_exists)
-    parser.add_argument("programtrace", type=file_exists)
-    parser.add_argument("page_size", type=int, choices=[2**i for i in range(5)], help="page size")
-    parser.add_argument("algorithm", choices=["clock", "fifo", "lru"], help="page replacement algorithm")
-    parser.add_argument("paging", type=int, choices=[0, 1], help="demand/prepaging")
-
-    args = parser.parse_args()
-
-	#makes args into global vars
-    global page_size, algo, paging
-    page_size = args.page_size
-    algo = args.algorithm
-    paging = args.paging
-    
-    with open(args.programlist) as f:
-        # index == prog number. filter blank lines
-        programlist = [int(line.split()[1]) for line in f if line.strip()]
-
-	#test code for 
-    page_tables = create_page_tables(programlist, args.page_size)
-    print_page_tables(page_tables, args.page_size)
-    
-	#test code for the main memory table
-    mem_table = create_mem(page_tables)
-    print_mem_table(mem_table)
-
-#Print fuctions for testing
 def print_page_tables(page_tables, page_size):
     print("{}\t{}\t{}\t{}\t{}".format("no", "size", "page", "need", "loc"))
     for p in page_tables:
         print("{}\t{}\t{}\t{}\t{} - {}".format(p.prog_no, p.prog_size, page_size, p.pages_needed, p.start_loc, p.end_loc))
 
-def print_mem_table(page_tables):
+def print_mem_table(mem_table):
     print("{}\t{}\t{}\t{}\t{}".format("program", "rel loc", "loc", "usebit", "active clocks"))
-    for p in page_tables:
-        print("{}\t{}\t{}\t{}".format(p.prognum, p.relprogpage, p.location, p.usebit, p.tsr))
+    for p in mem_table:
+        print("{} {}\t{}\t{}\t{}".format(p.index, p.prognum, p.relprogpage, p.location, p.usebit, p.tsr))
+
+# argparse "type" to determine if a file exists
+def file_exists(filename):
+    if not isfile(filename):
+        raise argparse.ArgumentTypeError("{} does not exist".format(filename))
+
+    return filename
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description = "Simulate a virtual memory management system with various page sizes, paging replacement algorithms, and demand/prepaging.")
+
+    # argparse performs all of the necessary checks on input formatting
+    parser.add_argument("programlist", type=file_exists, help="programlist file")
+    parser.add_argument("programtrace", type=file_exists, help="programtrace file")
+    parser.add_argument("page_size", type=int, choices=[2**i for i in range(5)], help="page size")
+    parser.add_argument("algorithm", choices=["clock", "fifo", "lru"], help="page replacement algorithm")
+    parser.add_argument("paging", type=int, choices=[0, 1], help="demand/prepaging")
+
+    args = parser.parse_args()
+    main(args.programlist, args.programtrace, args.page_size, args.algorithm, args.paging)
